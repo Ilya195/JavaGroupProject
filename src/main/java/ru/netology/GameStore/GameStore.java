3e039287b30a48622bcd1dba51d52fb9e2c34625
SergKry <necroastral@mail.ru>
2022-07-22 00:58:49 +0400

    исправление getSumPlayedTime

--- src/main/java/ru/netology/GameStore/GameStore.java
+++ src/main/java/ru/netology/GameStore/GameStore.java
@@ -73,6 +73,10 @@ public class GameStore {
      * за играми этого каталога
      */
     public int getSumPlayedTime() {
-        return 0;
+        int total = 0;
+        for (String playerName : playedTime.keySet()) {
+            total += playedTime.get(playerName);
+        }
+        return total;
     }
 }
